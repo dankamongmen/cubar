1327c47943cdf5b1f3fa1ebf734c7963d13cbe92
nick black <dank@qemfd.net>
2012-05-01 05:28:31 -0400

    dh_make output. support debian packaging

---
+++ debian/cubar.default.ex
@@ -0,0 +1,10 @@
+# Defaults for cubar initscript
+# sourced by /etc/init.d/cubar
+# installed at /etc/default/cubar by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(cubar):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="cubar" command="/usr/bin/cubar"
