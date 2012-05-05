2a5bff6e30ad42046b8687aed507aa461e68cc29
nick black <dank@qemfd.net>
2012-05-05 11:00:45 -0400

    dch for 41.2. add copyright (GPL2)

--- debian/cubar.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the cubar package
-#
-0 4	* * *	root	[ -x /usr/bin/cubar_maintenance ] && /usr/bin/cubar_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(cubar):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="cubar" command="/usr/bin/cubar"
